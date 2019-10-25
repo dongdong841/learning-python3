54085e2851fd9224bfd4aec6ce09459a1befc466
zhangtianyu <dongdong_841@163.com>
2019-10-25 16:04:57 +0800

    update
    done

--- project/Hangman/hangman.py
+++ project/Hangman/hangman.py
@@ -55,4 +55,32 @@ while True:
     guess = get_guess(missed_letters + correct_letters)
 
     if guess in secret_word:
-        correct_letters = 
+        correct_letters = correct_letters + guess
+
+        found_all_letters = True
+        for i in range(len(secret_word)):
+            if secret_word[i] not in correct_letters:
+                found_all_letters = False
+                break
+        if found_all_letters:
+            print('Yes! The secret word is "' + secret_word + '"! You have won!')
+            game_is_done = True
+    else:
+        missed_letters = missed_letters + guess
+
+        if len(missed_letters) == len(pics.HANGMAN_PICS) - 1:
+            display_board(missed_letters, correct_letters, secret_word)
+            print('You have run out of guesses!\nAfter '+
+                  str(len(missed_letters)) + ' missed guesses and ' +
+                  str(len(correct_letters)) + 'correct guesses, the word was "' +
+                  secret_word + '"')
+            game_is_done = True
+
+    if game_is_done:
+        if play_again():
+            missed_letters = ''
+            correct_letters = ''
+            game_is_done = False
+            secret_word = get_random_word(words.words)
+        else:
+            break
